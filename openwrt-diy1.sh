73c29154ce531a3750c3a8b3283f682883c9ca7c
hpqztsc <49110606+hpqztsc@users.noreply.github.com>
2024-01-08 17:37:20 +0800

    Update openwrt-diy1.sh

--- openwrt-diy1.sh
+++ openwrt-diy1.sh
@@ -17,16 +17,8 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 # 下载新主题
-# rm -rf ./package/lean/autocore
-# git clone -b lede https://github.com/supsupsuperstar/autocore.git package/lean/autocore
+git clone -b master https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
 
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
-#git clone -b 18.06 https://github.com/r1172464137/luci-theme-edge.git package/luci-theme-edge
-#git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/lean/luci-jd-dailybonus
 sed -i '$a src-git smallpackage https://github.com/kenzok8/small-package' feeds.conf.default
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 #sed -i '$a src-git jell https://github.com/kenzok8/jell' feeds.conf.default
-
-# 删除旧主题
-rm -rf feeds/kenzo/luci-theme-argon
-rm -rf package/lean/luci-theme-argon
